587ee1035a43671cbb5265d7efddc48e2a75381b
goodoletony <anthony.e.mendonca@gmail.com>
2025-12-29 13:48:41 -0800

    Build site into docs for GitHub Pages

--- src/generation.py
+++ src/generation.py
@@ -2,7 +2,7 @@ import os
 from extraction import extract_title
 from blocks import markdown_to_html_node
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, base_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     from_path = os.path.abspath(from_path)
     template_path = os.path.abspath(template_path)
@@ -18,6 +18,9 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(md_file)
     new_template = template.replace("{{ Title }}", title)
     new_template = new_template.replace("{{ Content }}", html_string)
+    new_template = new_template.replace('href="/', f'href="{base_path}"')
+    new_template = new_template.replace('src="/', f'src="{base_path}"')
+
     dest_dir = os.path.dirname(dest_path)
 
     if dest_dir:
@@ -27,7 +30,7 @@ def generate_page(from_path, template_path, dest_path):
         g.write(new_template)
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, base_path):
     for file in os.listdir(dir_path_content):
         src_path = os.path.join(dir_path_content, file)
         temp_path = os.path.abspath(template_path)
@@ -36,16 +39,16 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
             rel_path = os.path.relpath(src_path, dir_path_content)
             dest_path = os.path.join(dest_dir_path, rel_path).replace(".md", ".html")
             print(f"File Path: {src_path}\nTemplate Path: {temp_path}\nDestination Path: {dest_path}")
-            generate_page(src_path, temp_path, dest_path)
+            generate_page(src_path, temp_path, dest_path, base_path)
         else:
             rel_path = os.path.relpath(src_path, dir_path_content)
             dest_subdir = os.path.join(dest_dir_path, rel_path)
             print(f"Checking Dir: {src_path}\n")
             if os.path.isdir(dest_subdir):
-                generate_pages_recursive(src_path, temp_path, dest_subdir)
+                generate_pages_recursive(src_path, temp_path, dest_subdir, base_path)
             else:
                 os.makedirs(dest_subdir, exist_ok=True)
-                generate_pages_recursive(src_path, temp_path, dest_subdir)
+                generate_pages_recursive(src_path, temp_path, dest_subdir, base_path)
 
 
 # generate_pages_recursive("content", "template.html", "public/index.html")
--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
+import sys
 from copy_static import replace_public
 from generation import generate_pages_recursive
 
+default_basepath = "/"
 
 def main():
     replace_public()
-    generate_pages_recursive('content', 'template.html', 'public')
+    base_path = default_basepath
+    if len(sys.argv) > 1:
+        base_path = sys.argv[1]
+    generate_pages_recursive('content', 'template.html', 'docs', base_path)
 
 
 main()
